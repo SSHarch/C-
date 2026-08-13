6466d7b3514bb5a053389ee9b126ba79ab809504
SSH <tensai.future@gmail.com>
2026-08-13 11:11:17 +0300

    Add files via upload

---
+++ GraphicsManipulation/attack/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="background" tilewidth="240" tileheight="160" tilecount="1" columns="1">
+ <image source="../assets/tiles/background.png" width="240" height="160"/>
+</tileset>
