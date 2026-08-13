115623d702675142f33d463515a1c30856f122dd
SSH <tensai.future@gmail.com>
2026-08-13 11:12:04 +0300

    Add files via upload

---
+++ GraphicsManipulation/attack/tiled/goblin_idle_anim_strip_4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="goblin_idle_anim_strip_4" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="../assets/enemies/goblin/goblin_idle_anim_strip_4.png" width="64" height="16"/>
+</tileset>
---
+++ GraphicsManipulation/attack/tiled/tileset_32x32(new).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="tileset_32x32(new)" tilewidth="32" tileheight="32" tilecount="72" columns="12">
+ <image source="../assets/tiles/tileset_32x32(new).png" width="384" height="192"/>
+</tileset>
