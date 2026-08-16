074f3346b1f1ba6b8946457a8df56d9653e0d0a2
SSH <tensai.future@gmail.com>
2026-08-16 14:50:33 +0300

    Update eveorodd.c

--- DataStructures&Algorithms/eveorodd.c
+++ DataStructures&Algorithms/eveorodd.c
@@ -1,13 +1,22 @@
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
+
+bool isEven(int n) {
+  if ((n & 1) == 0)
+    return true;
+  else
+    return false;
+}
 
 int main() {
   int n;
   printf("Enter a number: ");
   scanf("%d", &n);
-  if (n % 2 > 0) {
-    printf("false\n");
-  } else if (n % 2 <= 0) {
+  if (isEven(n)) {
     printf("true\n");
+  } else {
+    printf("false\n");
   }
   return 0;
 }
