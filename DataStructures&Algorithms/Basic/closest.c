2b6e6a9936ec003bde61f80dbf76f1a49432512b
SSH <tensai.future@gmail.com>
2026-08-20 22:28:37 +0300

    Add files via upload

--- DataStructures&Algorithms/Basic/closest.c
+++ DataStructures&Algorithms/Basic/closest.c
@@ -1,16 +1,19 @@
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  int n = 13;
-  int m = 4;
+  int n = -15;
+  int m = 6;
   int c = m;
   int k;
   for (int i = 1; i <= n; i++) {
-    if (i % m == 0 && c > n - i) {
+    if (i % m == 0 && abs(c) > abs(n - i)) {
       c = n - i;
       k = i;
     }
   }
   printf("%d", k);
+  printf("%d", c);
   return 0;
 }
